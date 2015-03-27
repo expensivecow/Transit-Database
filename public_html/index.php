92ac6dcf3ee624e222af349c26cd66ae5da668f4
chrisyee1 <chrisyee@live.com>
2015-03-26 19:12:05 -0700

    Changes

--- public_html/index.php
+++ public_html/index.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<meta name="description" content>
-	<meta name="author" content>
+  <meta name="description" content>
+  <meta name="author" content>
 
     <title>CPSC304 Project</title>
     <!-- Bootstrap -->
     <link href="./css/bootstrap.min.css" rel="stylesheet">
     <link href="starter-template.css" rel="stylesheet">
-<style></style>	
+<style></style> 
 
   </head>
   <body>
@@ -43,10 +43,10 @@
       <div class="starter-template">
         <h1>CPSC304</h1>
         <p class="lead">This is the home page of h3g8's CPSC 304 group project.</p>
-	<p class="lead">Click the type of user which corresponds to you:</p>
-	<p class="lead"><a href="./managerLogin.php"> Manager </a></p>
-	<p class="lead"><a href="./employeeLogin.php"> Employee </a></p>
-	<p class="lead"><a href="./customerLogin.php"> Customers </a></p>
+  <p class="lead">Click the type of user which corresponds to you:</p>
+  <p class="lead"><a href="./managerLogin.php"> Manager </a></p>
+  <p class="lead"><a href="./employeeLogin.php"> Employee </a></p>
+  <p class="lead"><a href="./customerLogin.php"> Customers </a></p>
       </div>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -56,65 +56,6 @@
   </body>
 <?php else:?>
 
-
-<?php
-
-$success = True; //keep track of errors so it redirects the page only if there are no errors
-$db_conn = OCILogon("ora_p2n8", "a36523124", "ug");
-
-function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-  //echo "<br>running ".$cmdstr."<br>";
-  global $db_conn, $success;
-  $statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-  if (!$statement) {
-    echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-    $e = OCI_Error($db_conn); // For OCIParse errors pass the       
-    // connection handle
-    echo htmlentities($e['message']);
-    $success = False;
-  }
-
-  $r = OCIExecute($statement, OCI_DEFAULT);
-  if (!$r) {
-    echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-    $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-    echo htmlentities($e['message']);
-    $success = False;
-  } else {
-
-  }
-  return $statement;
-
-}
-// Connect Oracle...
-if ($db_conn) {
-      $result = executePlainSQL("select credit from customers where username = '$_SESSION['username']'");
-      $numrows = oci_fetch_all($result, $res);
-      if($numrows == 0){
-        echo "Invalid User or Password.";
-      }
-      if($numrows == 1){
-        $_SESSION['username'] = $users;
-        echo $_SESSION['username'];
-        $_SESSION['permissions'] = "USER";
-        echo $_SESSION['permissions'];
-        header("location: index.php");
-      }
-      //echo "Printing number of items: " . $result;
-    }
-    OCICommit($db_conn);
-
-    //Commit to save changes...
-    OCILogoff($db_conn);
-   // printResult($result);
-    //Commit to save changes...
-} else {
-  echo "cannot connect";
-  $e = OCI_Error(); // For OCILogon errors pass no handle
-  echo htmlentities($e['message']);
-}
-?>
 
 
     <head>
@@ -147,13 +88,12 @@ if ($db_conn) {
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="#Schedule">Schedule</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
             <li><a href="register.php">Register</a></li>
             <li><a href="http://www.cs.ubc.ca/~laks/cpsc304/project.html">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#contact">Balance</a></li>
             <li><a href="signout.php">Sign Out</a></li>
           </ul>
         </div><!--/.nav-collapse -->
@@ -161,7 +101,20 @@ if ($db_conn) {
     </nav>
     <div class="container">
       <div class="starter-template">
-        <?php echo "<center><p class='lead'>Welcome ". $_SESSION['username'] . "</p></center>";?>
+        <font size ="4"><?php echo "<center><p class='lead2'>Welcome ". $_SESSION['username'] . "</p></center>";?></font>
+        <font size ="7"><b>
+        <?php
+        echo "Balance is $";
+        $usern = $_SESSION['username'];
+        $conn = oci_connect("ora_p2n8", "a36523124", "ug");
+        $results = oci_parse($conn, "select credit from customers where username = '$usern'");
+        oci_execute($results);
+        while ($row = oci_fetch_array($results, OCI_BOTH)) {
+          echo "<tr><td>" . " " . $row["CREDIT"] . " </td></tr>";
+        }
+        oci_close($conn);
+        ?>
+        </font></b>
       </div>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
